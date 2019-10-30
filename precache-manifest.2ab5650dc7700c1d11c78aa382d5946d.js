674db52e6bb4484fd336ff662e7dea43c451ab9c
Bartosz Adamik <bartosz.adamik@idium.no>
2019-10-30 15:52:02 +0100

    Updates

---
+++ precache-manifest.2ab5650dc7700c1d11c78aa382d5946d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2977d2bd87ea24d2593f3728c90a0866",
+    "url": "/recruitment-task-ig/index.html"
+  },
+  {
+    "revision": "5a335fe3a12393853ab8",
+    "url": "/recruitment-task-ig/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "fc68b48f2c28f081b912",
+    "url": "/recruitment-task-ig/static/css/main.0eaba18e.chunk.css"
+  },
+  {
+    "revision": "5a335fe3a12393853ab8",
+    "url": "/recruitment-task-ig/static/js/2.836c49c2.chunk.js"
+  },
+  {
+    "revision": "fc68b48f2c28f081b912",
+    "url": "/recruitment-task-ig/static/js/main.14b5dff8.chunk.js"
+  },
+  {
+    "revision": "1197ddb72894ef77b2b3",
+    "url": "/recruitment-task-ig/static/js/runtime-main.8d378ecc.js"
+  }
+]);
